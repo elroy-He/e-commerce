6883f3ef42da40a9e45bfed09de31070e9550da9
elroy-He <elroyhsc@gmail.com>
2022-09-14 16:45:53 -0700

    finshed categories container

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./categories.styles.scss";
+//import CategoryItem from "./components/category-item/category-item.component";
+import CategoriesContainer from "./components/categories-container/categories-container.component";
+
+const App = () => {
+  return <CategoriesContainer />;
+};
+
+export default App;
